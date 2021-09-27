27725dec3f066da7bcfd5d87b75a79d09a634243
Zach Kellen <z.kellen12@gmail.com>
2021-09-27 16:48:24 -0500

    My First Java Program - complete

---
+++ FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("My name is Zach Kellen");
+        System.out.println("I am 25 years old");
+        System.out.println("My hometown is Fargo, ND");
+    }
+}
